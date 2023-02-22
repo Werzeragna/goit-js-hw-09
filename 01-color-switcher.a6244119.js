bea83edfa565ba82333dcf9e61543f5446c13571
Werzeragna <113331613+Werzeragna@users.noreply.github.com>
2023-02-22 22:14:37 +0000

    Deploying to gh-pages from @ Werzeragna/goit-js-hw-09@028f1489a351dd5789988ad6dc39c653faf00937 🚀

---
+++ 01-color-switcher.a6244119.js
@@ -0,0 +1,2 @@
+const t=document.querySelector("button[data-start]"),e=document.querySelector("button[data-stop]"),n=document.querySelector("body");e.disabled=!0;let o=null;t.style.fontSize="28px",e.style.fontSize="28px",t.style.fontWeight="600",e.style.fontWeight="600",t.addEventListener("click",(function(){t.disabled=!0,e.disabled=!1,o=setInterval((()=>{n.style.backgroundColor=`#${Math.floor(16777215*Math.random()).toString(16)}`}),1e3)})),e.addEventListener("click",(function(){t.disabled=!1,e.disabled=!0,clearInterval(o)}));
+//# sourceMappingURL=01-color-switcher.a6244119.js.map
